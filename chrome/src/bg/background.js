a3e1f6b0c73a0d390ea0d75306cd4e9716fadfcf
tingwai-to <tingwai.to@gmail.com>
2018-06-01 01:35:19 -0700

    Added logic to modify tab title and include relative position

--- chrome/src/bg/background.js
+++ chrome/src/bg/background.js
@@ -19,8 +19,8 @@ function selectTab(offset) {
 }
 
 function getActiveTabInfo(tabs) {
-    for (var tabIndex in tabs) {
-        var tab = tabs[tabIndex];
+    for (let tabIndex in tabs) {
+        let tab = tabs[tabIndex];
         if (tab.active) {
             return [parseInt(tabIndex), tab.id]
         }
@@ -41,6 +41,59 @@ let handleAction = (action, request = {}) => {
     }
 };
 
+let prependTabPosition = (tab, number) => {
+    let id = tab.id;
+    let title = tab.title;
+
+    // expression for starts with numbers, followed by period and space
+    regex_exp = "^[0-9]*[.][\\s]";
+    if (title.match(regex_exp)) {
+        // remove leading number, period, space
+        title = title.substr(title.indexOf('. ') + 2)
+    }
+
+    // prepend tab position
+    title = number + '. ' + title;
+
+    // modify tab title
+    try {
+        chrome.tabs.executeScript(
+            id,
+            {code: "document.title = '" + title + "';"}
+        )
+    } catch (e) {
+    }
+};
+
+function updateAllTabs() {
+    chrome.tabs.query({'currentWindow': true}, function (tabs) {
+        activeTab = getActiveTabInfo(tabs);
+        activeIndex = activeTab[0];
+
+        for (let i = 0; i < tabs.length; i++) {
+            prependTabPosition(tabs[i], Math.abs(activeIndex - i))
+        }
+    });
+}
+
 chrome.commands.onCommand.addListener(function (command) {
     handleAction(command)
 });
+
+chrome.tabs.onMoved.addListener(function () {
+    updateAllTabs();
+});
+
+chrome.tabs.onRemoved.addListener(function () {
+    updateAllTabs();
+});
+
+chrome.tabs.onActivated.addListener(function () {
+    updateAllTabs();
+});
+
+chrome.tabs.onUpdated.addListener(function () {
+    updateAllTabs();
+});
+
+updateAllTabs();
